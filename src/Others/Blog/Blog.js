d0f7ca2e6a9f5727133685404d245d93d02318d7
aminulhaque98 <aminulhaque419@gmail.com>
2022-12-07 04:11:06 +0530

    modify faq and  blog cmpnt

--- src/Others/Blog/Blog.js
+++ src/Others/Blog/Blog.js
@@ -3,29 +3,29 @@ import React from 'react';
 const Blog = () => {
     return (
         <div>
-            <div>
-                <h1>what is cors?</h1>
+            <div className="shadow-lg p-3 mb-5 bg-body rounded">
+                <h3>what is cors?</h3>
                 <p>
                     Cross-origin resource sharing (CORS) is a browser security feature that restricts cross-origin HTTP requests that are initiated from scripts running in the browser. If your REST API's resources receive non-simple cross-origin HTTP requests, you need to enable CORS support.
                 </p>
             </div>
 
-            <div>
-                <h1>Why are you using firebase? What other options do you have to implement authentication?</h1>
+            <div className="shadow-lg p-3 mb-5 bg-body rounded">
+                <h3>Why are you using firebase? What other options do you have to implement authentication?</h3>
                 <p>
                     Firebase Authentication provides backend services, easy-to-use SDKs, and ready-made UI libraries to authenticate users to your app. It supports authentication using passwords, phone numbers, popular federated identity providers like Google, Facebook and Twitter, and more.
                 </p>
             </div>
 
-            <div>
-                <h1>How does the private route work?</h1>
+            <div className="shadow-lg p-3 mb-5 bg-body rounded">
+                <h3>How does the private route work?</h3>
                 <p>
                     The private route component is similar to the public route, the only change is that redirect URL and authenticate condition. If the user is not authenticated he will be redirected to the login page and the user can only access the authenticated routes If he is authenticated (Logged in)
                 </p>
             </div>
 
-            <div>
-                <h1>What is Node? How does Node work?</h1>
+            <div className="shadow-lg p-3 mb-5 bg-body rounded">
+                <h3>What is Node? How does Node work?</h3>
                 <p>
                     Node. js is an open-source backend javascript runtime environment. It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend.
                 </p>
--- src/Others/FAQ/Faq.js
+++ src/Others/FAQ/Faq.js
@@ -1,34 +1,37 @@
 import React from 'react';
+import { FaClock } from 'react-icons/fa';
 
 const Faq = () => {
     return (
         <div>
 
-            <div>
-                <h1>What is the tutorial's duration?</h1>
+            <div className="shadow-lg p-3 mb-5 bg-body rounded">
+                <h3>What is the tutorial's duration?</h3>
                 <p>
                     approximately 5-6 month
                 </p>
             </div>
 
-            <div>
-                <h1>Support time per day</h1>
+            <div className="shadow-lg p-3 mb-5 bg-body rounded">
+                <h3>Support time per day</h3>
                 <p>
-                    At morning 11 am - 1.00 pm
-                    At evening (4-6) pm
-                    At night (9-11) pm
+                    <p> <FaClock></FaClock> At morning 11 am - 1.00 pm </p>
+                    <p> <FaClock></FaClock> At evening (4-6) pm</p>
+                    <p> <FaClock></FaClock> At night (9-11) pm</p>
+
+
                 </p>
             </div>
 
-            <div>
-                <h1>Assignment's</h1>
+            <div className="shadow-lg p-3 mb-5 bg-body rounded">
+                <h3>Assignment's</h3>
                 <p>
                     Every student must do 12 assignment ,and per assignment should be average marks 50% .
                 </p>
             </div>
 
-            <div>
-                <h1>Our team for support</h1>
+            <div className="shadow-lg p-3 mb-5 bg-body rounded">
+                <h3>Our team for support</h3>
                 <p>
                     We have team of 300 members for support
                 </p>
